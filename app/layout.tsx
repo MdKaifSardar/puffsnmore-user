af2c4657c9b7c34332ed4719a3764cd2859f047b
Raghunadh9 <konidoniravirr@gmail.com>
2024-11-21 20:25:02 +0530

    Updated search modal by connected to database

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Navbar from "@/components/shared/navbar/Navbar";
 import Footer from "@/components/shared/Footer";
 import MobileBottomBar from "@/components/shared/MobileBottomBar";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
         {children}
         <MobileBottomBar />
         <Footer />
+        <Toaster />
       </body>
     </html>
   );
