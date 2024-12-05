bf838309ebd0d2c0ae1e5051d75bb03eee5f6657
Raghunadh9 <konidoniravirr@gmail.com>
2024-12-05 20:07:46 +0530

    Updated Loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,7 +4,8 @@ export default function Loading() {
   // Or a custom loading skeleton component
   return (
     <div className="bg-opacity-20 flex justify-center items-center h-screen">
-      <Loader className="animate-spin" size={100} />
+      <Loader className="animate-spin" size={30} />
+      <p className="text-gray-400">Loading...</p>
     </div>
   );
 }
